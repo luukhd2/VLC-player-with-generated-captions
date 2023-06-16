e5d2cf0fbf408d8360f5d75a48d5e8773efc8243
luukhd2 <86712767+luukhd2@users.noreply.github.com>
2023-06-16 19:03:47 +0200

    Whisper command that changes log realtime

---
+++ whisper_transcribe.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import os
+import subprocess, sys
+
+def this_works():
+    ## command to run - tcp only ##
+    cmd = "echo 'test'; sleep 10; echo 'test2'"
+    
+    ## run it ##
+    p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+    import time
+    ## But do not wait till netstat finish, start displaying output immediately ##
+    while True:
+        for x in range(0, 1000):
+            print(x)
+            time.sleep(1)
+        out = p.stderr.read(1)
+        if out == '' and p.poll() != None:
+            break
+        if out != '':
+            sys.stdout.write(out)
+            sys.stdout.flush()
+
+
+def get_whisper_string(path_to_audio_file, model="tiny", language="English"):
+    """
+    'af', 'am', 'ar', 'as', 'az', 'ba', 'be', 'bg', 'bn', 'bo', 'br', 'bs', 'ca', 'cs', 'cy', 'da', 'de', 'el',
+    'en', 'es', 'et', 'eu', 'fa', 'fi', 'fo', 'fr', 'gl', 'gu', 'ha', 'haw', 'he', 'hi', 'hr', 'ht', 'hu', 'hy', 'id', 
+    'is', 'it', 'ja', 'jw', 'ka', 'kk', 'km', 'kn', 'ko', 'la', 'lb', 'ln', 'lo', 'lt', 'lv', 'mg', 'mi', 'mk', 'ml', 'mn', 
+    'mr', 'ms', 'mt', 'my', 'ne', 'nl', 'nn', 'no', 'oc', 'pa', 'pl', 'ps', 'pt', 'ro', 'ru', 'sa', 'sd', 'si', 'sk', 'sl', 'sn', 
+    'so', 'sq', 'sr', 'su', 'sv', 'sw', 'ta', 'te', 'tg', 'th', 'tk', 'tl', 'tr', 'tt', 'uk', 'ur', 'uz', 'vi', 'yi', 'yo', 
+    'zh', 'Afrikaans', 'Albanian', 'Amharic', 'Arabic', 'Armenian', 'Assamese', 'Azerbaijani', 'Bashkir', 'Basque', 
+    'Belarusian', 'Bengali', 'Bosnian', 'Breton', 'Bulgarian', 'Burmese', 'Castilian', 'Catalan', 'Chinese',
+      'Croatian', 'Czech', 'Danish', 'Dutch', 'English', 'Estonian', 'Faroese', 'Finnish', 'Flemish', 'French', '
+      Galician', 'Georgian', 'German', 'Greek', 'Gujarati', 'Haitian', 'Haitian Creole', 'Hausa', 'Hawaiian', 'Hebrew', 
+      'Hindi', 'Hungarian', 'Icelandic', 'Indonesian', 'Italian', 'Japanese', 'Javanese', 'Kannada', 'Kazakh', 'Khmer', 'Korean', 
+      'Lao', 'Latin', 'Latvian', 'Letzeburgesch', 'Lingala', 'Lithuanian', 'Luxembourgish', 'Macedonian', 'Malagasy', 'Malay', 'Malayalam', 
+      'Maltese', 'Maori', 'Marathi', 'Moldavian', 'Moldovan', 'Mongolian', 'Myanmar', 'Nepali', 'Norwegian', 'Nynorsk', 'Occitan', 'Panjabi', 
+      'Pashto', 'Persian', 'Polish', 'Portuguese', 'Punjabi', 'Pushto', 'Romanian', 'Russian', 'Sanskrit', 'Serbian', 'Shona', 'Sindhi',
+        'Sinhala', 'Sinhalese', 'Slovak', 'Slovenian', 'Somali', 'Spanish', 'Sundanese', 'Swahili', 'Swedish', 'Tagalog', 'Tajik',
+          'Tamil', 'Tatar', 'Telugu', 'Thai', 'Tibetan', 'Turkish', 'Turkmen', 'Ukrainian', 'Urdu', 'Uzbek', 'Valencian', 'Vietnamese', 
+          'Welsh', 'Yiddish', 'Yoruba'
+    """
+    return f"whisper '{path_to_audio_file}'  --model {model} --model_dir /Users/kcd635/Documents/GitHub/CaptionViewerNew/models/ --output_format srt --task transcribe --language {language}"
+
+
+class FileModified():
+    def __init__(self, file_path, callback):
+        self.file_path = file_path
+        self.callback = callback
+        self.modifiedOn = os.path.getmtime(file_path)
+
+
+with open("test.log", "wb") as f:
+    your_command = get_whisper_string("a.mp3", language='Russian')
+    print(your_command)
+    process = subprocess.Popen(your_command, stdout=subprocess.PIPE, shell=True)
+    for c in iter(lambda: process.stdout.read(1), b""):
+        sys.stdout.buffer.write(c)
+        f.write(c)
+    
